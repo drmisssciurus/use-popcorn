26c256585be63a8503eca7abc040e7cf50c714e3
Alina <ms.sciurus@gmail.com>
2025-01-07 13:48:17 +0200

    proptype was removed from react 19

--- src/index.js
+++ src/index.js
@@ -22,13 +22,7 @@ root.render(
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
     />
-    <StarRating
-      maxRating={10}
-      size={24}
-      color="red"
-      className="test"
-      defaultRating={3}
-    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
     <Test />
   </React.StrictMode>
 );
